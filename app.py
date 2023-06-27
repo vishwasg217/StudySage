faf6d9c4cf9e7068c1c98cbebd78e1bc799888ef
vishwasg217 <vishwas.g217@gmail.com>
2023-06-27 14:44:27 +0530

    chatbot feature added

--- app.py
+++ app.py
@@ -7,28 +7,37 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from streamlit_chat import message
+
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
 
 config = dotenv_values(".env")
 OPENAI_API_KEY = config["OPENAI_API_KEY"]
 
-llm = OpenAI(openai_api_key=OPENAI_API_KEY,temperature=0.9, model_name="gpt-3.5-turbo")
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+# llm = OpenAI(openai_api_key=OPENAI_API_KEY,temperature=0.9, model_name="gpt-3.5-turbo")
+# embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
 
 
 st.title("StudySage - Your AI Study Buddy")
 
-
-pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
-if pdf is not None:
+def get_pdf_text(pdf):
     pdf_reader = PdfReader(pdf)
     text = ""
     for page in pdf_reader.pages:
         text += page.extract_text()
+    return text
 
+def get_chunks(text):
     # split into chunks
     text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -37,49 +46,63 @@ if pdf is not None:
     length_function=len
     )
     chunks = text_splitter.split_text(text)
+    return chunks
 
+def vector_store(chunks):
     # create embeddings
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     knowledge_base = FAISS.from_texts(chunks, embeddings)
     with open("knowledge_base.pkl", "wb") as f:
         pickle.dump(knowledge_base, f)
+    return knowledge_base
 
-    # show user input
+def get_converation_chain(vector_store):
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=vector_store.as_retriever(), memory=memory)
+    return conversation_chain
 
-    # user_question = st.text_input("Ask a question about your PDF:")
-    user_question = st.text_input("Ask a question about your PDF:")
-    if user_question:
-        docs = knowledge_base.similarity_search(user_question)
+def handle_user_input(user_question):
+    st.session_state.chat_history.append(HumanMessage(content=user_input))
+    with st.spinner("Thinking..."):
+        response = st.session_state.conversation({'question': user_question})
+        # response = llm(st.session_state.chat_history)
+    st.session_state.chat_history.append(
+        AIMessage(content=response['answer']))
     
-        chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-            response = chain.run(input_documents=docs, question=user_question)
-            print(cb)
-            
-        st.write(response)
+    messages = st.session_state.get('chat_history', [])
+    for i, msg in enumerate(messages[1:]):
+        if i % 2 == 0:
+            message(msg.content, is_user=True, key=str(i) + '_user')
+        else:
+            message(msg.content, is_user=False, key=str(i) + '_ai')
 
-    prompt1 = st.text_input("topic")
 
-    prompt1_template = PromptTemplate(
-        input_variables=['topic'],
-        template="summarize the content in {topic}?", 
-    )
-    if prompt1:
-        docs = knowledge_base.similarity_search(prompt1)
 
-    
-        chain = load_qa_chain(llm, chain_type="stuff")
-        with get_openai_callback() as cb:
-            response = chain.run(input_documents=docs, question=prompt1)
-            print(cb)
+if "conversation" not in st.session_state:
+    st.session_state.conversation = None
+
+if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            SystemMessage(content="You are a helpful assistant.")
+        ]
+
+
+
+pdf = st.sidebar.file_uploader("Upload a PDF file", type=["pdf"])
+if st.sidebar.button("Process PDF"):
+    with st.spinner("Processing PDF..."):
+        text = get_pdf_text(pdf)
+        chunks = get_chunks(text)
+
+        # create embeddings
+        knowledge_base = vector_store(chunks)
+        st.session_state.conversation = get_converation_chain(knowledge_base)
+
+user_input = st.sidebar.text_input("Ask questions")
+if user_input:
+    handle_user_input(user_input)
         
-        st.write(response)
+    # st.session_state.conversation
 
 
-
-# title_chain = LLMChain(llm=llm, prompt=prompt1_template)
-
-# if prompt1:
-#     docs = knowledge_base.similarity_search(prompt1)
-#     response = title_chain.run(topic=prompt1, input_documents=docs)
-#     st.write(response)
+        
